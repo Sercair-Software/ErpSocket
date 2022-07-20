9dbc5ed64382d83e35fbeeb1788d4c24d4b794ba
onur-dogan <aindres@hotmail.com>
2022-07-20 18:02:31 +0300

    Socket

--- server.js
+++ server.js
@@ -30,8 +30,35 @@ const io = new Server(server, {
 const ChatRoomFunction = require('./functions/chatRoomFunction')
 const ChatRoomMessageFunction = require('./functions/chatRoomMessajeFunction')
 const ChatRoomEntity = require('./entities/chatRoom')
+const { default: axios } = require('axios')
+const convert = require('xml-js')
 io.on("connection", (socket) => {
     console.log("User Connected ", socket.id)
+    
+    socket.on("callApi_map_car", async() => {
+        socket.join("sercair")
+        var xmls = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.com/">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <web:PLITR1>\n                <a1>sercair</a1>\n                <a2>blink2014</a2>\n                <a3>213444</a3>\n                <o1>json</o1>\n      </web:PLITR1>\n   </soapenv:Body>\n</soapenv:Envelope>';
+        var config = {
+            method: 'post',
+            url: 'http://37.75.11.73:80/Service724/track724WS',
+            headers: {
+                'Access-Control-Allow-Origin': '',
+                "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+                'Content-Type': 'text/xml; charset=utf-8',
+                'Cookie': 'FGTServer=94295B2F3C8A09563B8E86D4B0F2A2FC9F6C8C76D04B4426FBAE6A81F015BFB287C7',
+            },
+            data: xmls,
+        }
+        await axios(config).then(async function (response) {
+            const res = JSON.parse(convert.xml2json(response.data, { compact: true, spaces: 2 }))
+            const data = await res['soap:Envelope']['soap:Body']['ns2:PLITR1Response'].return._text;
+            socket.emit('listen_car_map', data);
+        })
+            .catch(function (err) {
+                console.log("verihata", err)
+            })
+    })
 
     socket.on("create_room", async (data) => {
         const model = new ChatRoomEntity({
@@ -56,7 +83,7 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
         console.log("User Disconnected ", socket.id)
-    })
+    }) 
 })
 
 server.listen(3002, () => {
